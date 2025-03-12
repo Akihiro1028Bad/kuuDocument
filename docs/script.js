72902716c0caaf6b30cffb2d8a06428e586f5307
tsu-tsu-aki <a_tsutsumi@fujiball.co.jp>
2025-03-12 23:30:20 +0900

    修正

--- docs/script.js
+++ docs/script.js
@@ -1,26 +1,17 @@
 document.addEventListener('DOMContentLoaded', function() {
     const loadingScreen = document.getElementById('loading-screen');
 
-    // ローディング画面を非表示にする
+    // ローディング画面を非表示
     setTimeout(() => {
         loadingScreen.classList.add('hidden');
-         // Intersection Observer APIを使ってセクションをフェードイン
+
+        // フェードインアニメーション
         const sections = document.querySelectorAll('.section');
-
-        const observer = new IntersectionObserver(entries => {
-            entries.forEach(entry => {
-                if (entry.isIntersecting) {
-                    entry.target.classList.add('active');
-                    observer.unobserve(entry.target); // 一度表示されたら監視を停止
-                }
-            });
-        }, {
-            threshold: 0.2 // 20% 表示されたら
+        sections.forEach((section, index) => {
+            setTimeout(() => {
+                section.classList.add('fade-in');
+            }, 200 * index);
         });
 
-        sections.forEach(section => {
-            observer.observe(section);
-        });
-    }, 1500); // アニメーションが終わる時間に合わせる
-
+    }, 2500);
 });
