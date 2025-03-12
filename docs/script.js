70c139ccd83d6c9e29ec2974d03d5bef3a5a1551
tsu-tsu-aki <a_tsutsumi@fujiball.co.jp>
2025-03-12 23:11:14 +0900

    マージ

---
+++ docs/script.js
@@ -0,0 +1,26 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const loadingScreen = document.getElementById('loading-screen');
+
+    // ローディング画面を非表示にする
+    setTimeout(() => {
+        loadingScreen.classList.add('hidden');
+         // Intersection Observer APIを使ってセクションをフェードイン
+        const sections = document.querySelectorAll('.section');
+
+        const observer = new IntersectionObserver(entries => {
+            entries.forEach(entry => {
+                if (entry.isIntersecting) {
+                    entry.target.classList.add('active');
+                    observer.unobserve(entry.target); // 一度表示されたら監視を停止
+                }
+            });
+        }, {
+            threshold: 0.2 // 20% 表示されたら
+        });
+
+        sections.forEach(section => {
+            observer.observe(section);
+        });
+    }, 1500); // アニメーションが終わる時間に合わせる
+
+});
